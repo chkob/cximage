c4f55fe1f4b577d04bf48ea8966958169f573859
Chatchai Daecha <Chatchai.Daecha@agoda.com>
2016-09-18 16:35:09 +0700

    update libpng to ver.1.6.25

--- cximage/ximapng.h
+++ cximage/ximapng.h
@@ -85,7 +85,8 @@ protected:
     static void PNGAPI user_error_fn(png_structp png_ptr,png_const_charp error_msg)
 	{
 		strncpy((char*)png_ptr->error_ptr,error_msg,255);
-		longjmp(png_ptr->png_jmpbuf, 1);
+		//longjmp(png_ptr->png_jmpbuf, 1);
+		png_jmpbuf(png_ptr);
 	}
 };
 
